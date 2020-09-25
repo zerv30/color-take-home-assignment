7d17bd7c88852fca9c7ec02a75acb8830bc1c284
Zane Ervin <zaneerv@gmail.com>
2020-09-24 19:28:24 -0700

    finifshing touches

--- versionId.js
+++ versionId.js
@@ -3,22 +3,22 @@ const getVersionId = (major, minor, patch, optionalLabel) => {
     if (typeof (major) === 'undefined' || typeof (minor) === 'undefined' || typeof (patch) === 'undefined') {
         throw "Parameter(s) was(were) not passed"
     }
-    
+
     // Thorws an error if the major, minor, patch variables are not varibles
     if (!Number.isInteger(major) || !Number.isInteger(minor) || !Number.isInteger(patch)) {
         throw "major, minor, and patch need to be numbers"
     }
 
     // Throws an error if the optional label exists but isn't a string
-    if( optionalLabel){
-        if(typeof optionalLabel !== 'string') throw "The optional label was not a string"
-        if(!optionalLabel.match( /^[A-Za-z]+$/)) throw "Was not an alphabetival string"
+    if (optionalLabel !== undefined) {
+        if (typeof optionalLabel !== 'string') throw "The optional label is not a string"
+        if (!optionalLabel.match(/^[A-Za-z]+$/)) throw "The optional label is not an alphabetical string"
     }
 
     let versionId = `v${major}.${minor}.${patch}${optionalLabel ? `-${optionalLabel}` : ''}`
     return versionId
 };
 
-console.log(getVersionId(1,2,3,'beta'))
+console.log(getVersionId(1, 2, 3, 'beta'))
 
 module.exports = getVersionId
--- versionId.test.js
+++ versionId.test.js
@@ -1,40 +1,40 @@
-const  getVersionId = require("./versionId");
+const getVersionId = require("./versionId");
 
-test('properly combines major,minor and patch numbers', ()=>{
-    expect(getVersionId(1,3,5)).toBe('v1.3.5')
+test('properly combines major,minor and patch numbers', () => {
+    expect(getVersionId(1, 3, 5)).toBe('v1.3.5')
 })
 
-test('properly combines major,minor and patch numbers with the optional', ()=>{
-    expect(getVersionId(2,2,1,'alpha')).toBe('v2.2.1-alpha')
+test('properly combines major,minor and patch numbers with the optional', () => {
+    expect(getVersionId(2, 2, 1, 'alpha')).toBe('v2.2.1-alpha')
 })
 
 
-test('Throws an error if at least of the first three arguments are missing', ()=>{
+test('Throws an error if at least of the first three arguments are missing', () => {
 
     expect(() => getVersionId()).toThrow("Parameter(s) was(were) not passed");
     expect(() => getVersionId(2)).toThrow("Parameter(s) was(were) not passed");
-    expect(() => getVersionId(1,2)).toThrow("Parameter(s) was(were) not passed");
+    expect(() => getVersionId(1, 2)).toThrow("Parameter(s) was(were) not passed");
 })
 
 
 
 
-test('Throws an error if the major, minor, and/or patch variables aren\'t numbers  ', ()=>{
+test('Throws an error if the major, minor, and/or patch variables aren\'t numbers  ', () => {
 
-    expect(() => getVersionId('1','two','twelve')).toThrow("major, minor, and patch need to be numbers");
-
-})
-
-test('Throws an error if the optional label is not a string  ', ()=>{
-
-    expect(() => getVersionId(1,2,3,false)).toThrow("The optional label is not a string");
-    expect(() => getVersionId(1,2,3,12)).toThrow("The optional label is not a string");
+    expect(() => getVersionId('1', 'two', 'twelve')).toThrow("major, minor, and patch need to be numbers");
 
 })
 
+test('Throws an error if the optional label is not a string  ', () => {
 
-test('Throws an error if the optional label is not a string  ', ()=>{
+    expect(() => getVersionId(1, 2, 3, false)).toThrow("The optional label is not a string");
+    expect(() => getVersionId(1, 2, 3, 12)).toThrow("The optional label is not a string");
 
-    expect(() => getVersionId(1,2,3,'13')).toThrow("The optional label is not an alphabetical string");
-    
+})
+
+
+test('Throws an error if the optional label is a string that contains non-alphabetic characters ', () => {
+
+    expect(() => getVersionId(1, 2, 3, '13')).toThrow("The optional label is not an alphabetical string");
+
 })
